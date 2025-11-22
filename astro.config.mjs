0c04d95dc1f6cc4154398ea65d89769f4273915b
josegustavo <jose.quilca@goumh.umh.es>
2025-11-22 18:02:24 -0500

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import yaml from '@rollup/plugin-yaml';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://omegasalud.pe',
+
   vite: {
     plugins: [tailwindcss(), yaml()]
   },
-  integrations: [sitemap()]
+
+  integrations: [sitemap()],
+  adapter: cloudflare()
 });
